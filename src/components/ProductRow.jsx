385c454e185bb94730ec64d0b9af6f5384cb933c
hurtaditoo <al415507@uji.es>
2024-11-26 00:26:22 +0100

    Iteration 3 | Product Row

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,9 +1,15 @@
 
-function ProductRow() {
+function ProductRow({ product }) {
     return (
-        <div>
-            <h4>Product Row</h4>
-        </div>
+        <tr>
+            <td
+                className={product.id}
+                style={{ padding: "8px", borderBottom: "1px solid #ddd", color: product.inStock ? "black" : "red" }}
+            >
+                {product.name}
+            </td>
+            <td style={{ padding: "8px", borderBottom: "1px solid #ddd" }} >{product.price}</td>
+        </tr>
     );
 }
 
--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -2,21 +2,19 @@ import ProductRow from "./ProductRow";
 
 function ProductTable({ products }) {
     return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map((product) => (
-                        <ProductRow key={product.id} product={product} />
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <table style={{ width: "100%", borderCollapse: "collapse" }} >
+            <thead style={{ backgroundColor: "#f9f9f9" }} >
+                <tr style={{ padding: "8px", textAlign: "center", borderBottom: "1px solid #ddd" }} >
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map((product) => (
+                    <ProductRow key={product.id} product={product} />
+                ))}
+            </tbody>
+        </table>
     );
 }
 
